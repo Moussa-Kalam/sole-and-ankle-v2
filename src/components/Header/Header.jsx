e3020d6ebb68ef3b7a590836bae53b1babcb88f4
Moussa Kalam AMZAT <moussakalamamzat@gmail.com>
2025-01-20 00:25:34 +0200

    implement the main header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from '../../constants'
+import Logo from '../Logo'
+import SuperHeader from '../SuperHeader'
 
 const Header = () => {
   // Our site features two visual headers, but they should be
   // grouped semantically as a single header.
   return (
     <header>
-      <SuperHeader />
+      <SuperHeader/>
       <MainHeader>
-        <Logo />
+        <Box>
+          <Logo/>
+        </Box>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,28 +23,40 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Box/>
       </MainHeader>
     </header>
-  );
-};
+  )
+}
 
 const MainHeader = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
-`;
+    display: flex;
+    align-items: baseline;
+    padding: 24px 32px 0;
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    height: 72px;
+`
 
-const Nav = styled.nav``;
+const Box = styled.div`
+    flex: 1;
+`
+
+const Nav = styled.nav`
+    display: flex;
+    gap: 48px;
+    margin-inline: 40px;
+`
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
-`;
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
+`
 
-export default Header;
+export default Header
